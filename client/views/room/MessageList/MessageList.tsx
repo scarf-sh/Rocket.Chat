fc4e16ec2110923fc53928ca8685dcdba59ddd19
Guilherme Gazzo <guilherme@gazzo.xyz>
2021-05-10 14:27:33 -0300

    loadMore

--- client/views/room/MessageList/MessageList.tsx
+++ client/views/room/MessageList/MessageList.tsx
@@ -1,12 +1,13 @@
 import { Message as MessageTemplate } from '@rocket.chat/fuselage';
 import { isSameDay } from 'date-fns';
-import React, { FC } from 'react';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { Virtuoso } from 'react-virtuoso';
 
 import { IRoom } from '../../../../definition/IRoom';
+import RoomForeword from '../../../components/RoomForeword';
 import ScrollableContentWrapper from '../../../components/ScrollableContentWrapper';
 import { useFormatDateAndTime } from '../../../hooks/useFormatDateAndTime';
-import { useRoom } from '../providers/RoomProvider';
+import { useRoom, useRoomContext } from '../providers/RoomProvider';
 import { Message } from './components/Message';
 import { SequentialMessage } from './components/SequentialMessage';
 import { useMessages } from './hooks/useMessages';
@@ -14,32 +15,61 @@ import { isMessageSequential } from './libs/isMessageSequential';
 
 export const MessageList: FC = () => {
 	const room = useRoom() as IRoom;
+
+	const prepending = useRef(0);
+
 	const messages = useMessages({ rid: room._id });
 
 	const format = useFormatDateAndTime();
+	const { getMore } = useRoomContext();
 
+	const [firstItemIndex, setFirstItemIndex] = useState(messages.length);
+
+	const more = useCallback(() => {
+		prepending.current = messages.length;
+		getMore();
+	}, [getMore, messages.length]);
+
+	useEffect(() => {
+		if (prepending.current) {
+			setFirstItemIndex((old) => messages.length - old);
+			prepending.current = 0;
+		}
+	}, [messages.length]);
+
+	const itemContent = useCallback(
+		(_, message) => {
+			const index = messages.findIndex((m) => m === message);
+			const previous = messages[index - 1];
+
+			const sequential = isMessageSequential(message, previous);
+
+			const newDay = !previous || !isSameDay(message.ts, previous.ts);
+
+			const Template = sequential ? SequentialMessage : Message;
+			return (
+				<>
+					{newDay && <MessageTemplate.Divider>{format(message.ts)}</MessageTemplate.Divider>}
+					<Template message={message} key={message._id} />
+				</>
+			);
+		},
+		[messages],
+	);
 	return (
 		<Virtuoso
 			overscan={50}
+			firstItemIndex={firstItemIndex}
 			totalCount={messages.length}
 			data={messages}
-			components={{ Scroller: ScrollableContentWrapper as any }}
-			followOutput
-			itemContent={(index, message) => {
-				const previous = messages[index - 1];
-
-				const sequential = isMessageSequential(message, previous);
-
-				const newDay = !previous || !isSameDay(message.ts, previous.ts);
-
-				const Template = sequential ? SequentialMessage : Message;
-				return (
-					<>
-						{newDay && <MessageTemplate.Divider>{format(message.ts)}</MessageTemplate.Divider>}
-						<Template message={message} key={message._id} />
-					</>
-				);
+			defaultItemHeight={49}
+			components={{
+				Scroller: ScrollableContentWrapper as any,
+				Header: () => <RoomForeword rid={room._id} />,
 			}}
+			followOutput
+			startReached={more}
+			itemContent={itemContent}
 		/>
 	);
 };
